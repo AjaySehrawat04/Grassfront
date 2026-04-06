2a4f2192d232dac7f54e5e920d684613563dbc82
Ajay Sehrawat <asehrawat912@gmail.com>
2026-04-06 20:01:25 +0530

    feature/update

--- grassfront-main/tailwind.config.js
+++ grassfront-main/tailwind.config.js
@@ -1,5 +1,4 @@
-import type { Config } from "tailwindcss";
-
+/** @type {import('tailwindcss').Config} */
 export default {
   darkMode: ["class"],
   content: ["./pages/**/*.{ts,tsx,js,jsx}", "./components/**/*.{ts,tsx,js,jsx}", "./app/**/*.{ts,tsx,js,jsx}", "./src/**/*.{ts,tsx,js,jsx}"],
@@ -96,4 +95,4 @@ export default {
     },
   },
   plugins: [require("tailwindcss-animate")],
-} satisfies Config;
+};
